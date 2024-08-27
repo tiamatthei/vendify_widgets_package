7f674b170fbcb4fe438a4447f1b198593a091e8e
Jorge <coke200898@gmail.com>
2024-08-27 16:53:58 -0400

    Add step and color properties to ContactState class

--- lib/classes/contacts/contact_state.dart
+++ lib/classes/contacts/contact_state.dart
@@ -1,30 +1,44 @@
+import 'package:flutter/material.dart';
 import 'package:vendify_widgets_package/classes/contacts/contact_label.dart';
 
 class ContactState {
   final int contactStateId;
   final String state;
   final String description;
+  final int step;
+  final String color;
   List<ContactLabel>? linkedLabels;
 
   ContactState({
     required this.contactStateId,
     required this.state,
     required this.description,
+    required this.step,
+    required this.color,
     this.linkedLabels,
   });
 
   factory ContactState.fromJson(Map<String, dynamic> json) => ContactState(
-        contactStateId: json['contactStateId'],
+        contactStateId: json['contact_state_id'],
         state: json['state'],
         description: json['description'],
+        step: json['step'],
+        color: json['color'],
         linkedLabels: json['linkedLabels'] != null
             ? List<ContactLabel>.from(json['linkedLabels'].map((x) => ContactLabel.fromJson(x)))
             : null,
       );
 
   Map<String, dynamic> toJson() => {
-        'contactStateId': contactStateId,
+        'contact_state_id': contactStateId,
         'state': state,
         'description': description,
+        'step': step,
+        'color': color,
       };
+
+  Color get colorValue {
+    return Color(int.parse(color.substring(1, 7), radix: 16) + 0xFF000000);
+  }
+
 }
