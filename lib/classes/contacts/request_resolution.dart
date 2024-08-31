6b76a8c5ed82ed32a28a5d18b23257c3767f1f15
Jorge <coke200898@gmail.com>
2024-08-31 19:41:48 -0400

    Add isAccepted and tenantId properties to RequestResolution class

--- lib/classes/contacts/request_resolution.dart
+++ lib/classes/contacts/request_resolution.dart
@@ -1,16 +1,22 @@
 class RequestResolution {
   int? resolutionId;
   String? resolution;
+  bool? isAccepted;
+  int? tenantId;
 
-  RequestResolution({this.resolutionId, this.resolution});
+  RequestResolution({this.resolutionId, this.resolution, this.isAccepted, this.tenantId});
 
   factory RequestResolution.fromJson(Map<String, dynamic> json) => RequestResolution(
         resolutionId: json['resolutionId'],
         resolution: json['resolution'],
+        isAccepted: json['isAccepted'],
+        tenantId: json['tenantId'],
       );
 
   Map<String, dynamic> toJson() => {
         'resolutionId': resolutionId,
         'resolution': resolution,
+        'isAccepted': isAccepted,
+        'tenantId': tenantId,
       };
 }
