f59f28b30990395068da4b93a1df68544f2c4822
Jorge <coke200898@gmail.com>
2024-09-16 21:47:54 -0300

    Mejora text_button, agrega opción de botón terciario. Agrega simple_modal para alertas/modales simples con elementos simples

--- lib/widgets/text_button.dart
+++ lib/widgets/text_button.dart
@@ -1,16 +1,39 @@
 library text_button;
+
 import 'package:flutter/material.dart';
 import 'package:vendify_widgets_package/colors.dart';
 
-class SimpleButton extends StatefulWidget {
+enum SimpleButtonType { primary, secondary, tertiary, destructive, tertiaryDestructive, successDefault }
+
+class SimpleButton extends StatelessWidget {
+  /// Botón simple con texto e icono
+
+  /// Texto del botón
   final String? text;
+
+  /// Función que se ejecuta al presionar el botón
   final void Function()? onPressed;
+
+  /// Color de fondo del botón
   final Color? backgroundColor;
+
+  ///Color del texto del botón [text]
+  @Deprecated('Usar buttonType')
   final Color? textColor;
   final IconData? icon;
   final MainAxisSize mainAxisSize;
+
+  /// Tipo de botón, ver [SimpleButtonType]
+  final SimpleButtonType buttonType;
+
+  /// DEPRECATED: Si el botón es secundario
+  @Deprecated('Usar buttonType')
   final bool isSecondary;
+
+  /// Padding interno del botón
   final WidgetStateProperty<EdgeInsetsGeometry>? padding;
+
+  /// Si el botón está cargando, muestra un indicador de carga
   final bool isLoading;
 
   const SimpleButton({
@@ -18,75 +41,87 @@ class SimpleButton extends StatefulWidget {
     this.text,
     this.onPressed,
     this.backgroundColor,
-    this.textColor,
+    @Deprecated('Usar buttonType') this.textColor,
     this.icon,
     this.mainAxisSize = MainAxisSize.max,
-    this.isSecondary = false,
+    @Deprecated('Usar buttonType') this.isSecondary = false,
     this.padding,
     this.isLoading = false,
+    this.buttonType = SimpleButtonType.primary,
   });
 
   @override
-  State<SimpleButton> createState() => _SimpleButtonState();
-}
-
-class _SimpleButtonState extends State<SimpleButton> {
-  @override
   Widget build(BuildContext context) {
+    Color backgroundColorResolver() {
+      switch (buttonType) {
+        case SimpleButtonType.primary:
+          return TColors.accentDefault;
+        case SimpleButtonType.secondary || SimpleButtonType.tertiary || SimpleButtonType.tertiaryDestructive:
+          return TColors.backgroundDefault;
+        case SimpleButtonType.destructive:
+          return TColors.errorDefault;
+        case SimpleButtonType.successDefault:
+          return TColors.successDefault;
+      }
+    }
+
+    Color textColorResolver() {
+      switch (buttonType) {
+        case SimpleButtonType.primary || SimpleButtonType.destructive || SimpleButtonType.successDefault:
+          return Colors.white;
+        case SimpleButtonType.secondary || SimpleButtonType.tertiary:
+          return TColors.accentDefault;
+        case SimpleButtonType.tertiaryDestructive:
+          return TColors.errorDefault;
+      }
+    }
+
+    BorderSide borderResolver() {
+      switch (buttonType) {
+        case SimpleButtonType.secondary:
+          return const BorderSide(color: TColors.accentDefault, width: 2);
+        default:
+          return BorderSide.none;
+      }
+    }
+
     double width = MediaQuery.of(context).size.width;
     return Align(
         heightFactor: 1,
         widthFactor: 1,
         alignment: Alignment.center,
         child: TextButton(
-          onPressed: widget.onPressed,
+          onPressed: onPressed,
           style: ButtonStyle(
-              backgroundColor: WidgetStateProperty.all<Color>(
-                widget.backgroundColor ??
-                    (widget.isSecondary ? Colors.white : TColors.accentDefault),
-              ),
-              foregroundColor: WidgetStateProperty.all<Color>(
-                widget.textColor ??
-                    (widget.isSecondary ? TColors.accentDefault : Colors.white),
-              ),
+              backgroundColor: WidgetStateProperty.all<Color>(backgroundColorResolver()),
+              foregroundColor: WidgetStateProperty.all<Color>(textColorResolver()),
               shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    side: widget.isSecondary
-                        ? const BorderSide(
-                            color: TColors.accentDefault, width: 2)
-                        : BorderSide.none),
-              ),
+                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(10), side: borderResolver())),
               maximumSize: WidgetStateProperty.all<Size>(Size(width, 50)),
-              padding: widget.padding),
+              padding: padding),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: widget.mainAxisSize,
-            children: widget.isLoading
+            mainAxisSize: mainAxisSize,
+            children: isLoading
                 ? [
                     SizedBox(
                       width: 18,
                       height: 18,
                       child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(
-                          widget.isSecondary
-                              ? TColors.accentDefault
-                              : Colors.white,
-                        ),
+                        valueColor: AlwaysStoppedAnimation<Color>(textColorResolver()),
                       ),
                     )
                   ]
                 : [
-                    if (widget.text != null) Text(widget.text!),
-                    if (widget.icon != null)
+                    if (text != null)
+                      Text(text!, style: Theme.of(context).textTheme.labelMedium!.copyWith(color: textColorResolver())),
+                    if (icon != null)
                       Padding(
-                          padding: widget.text != null
-                              ? const EdgeInsets.only(left: 8)
-                              : EdgeInsets.zero,
+                          padding: text != null ? const EdgeInsets.only(left: 8) : EdgeInsets.zero,
                           child: Icon(
-                            widget.icon,
-                            color: widget.textColor ?? Colors.white,
+                            icon,
+                            color: textColorResolver(),
                           ))
                   ],
           ),
