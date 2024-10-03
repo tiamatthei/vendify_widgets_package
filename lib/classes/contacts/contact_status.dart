1b3976a7075fb3f645c6a82a21122c8aa46d4788
Jorge <coke200898@gmail.com>
2024-10-02 23:51:22 -0300

    Camelcasing contact_status and fixing button type in user card

--- lib/classes/contacts/contact_status.dart
+++ lib/classes/contacts/contact_status.dart
@@ -13,19 +13,19 @@ class ContactStatus {
 
   factory ContactStatus.fromJson(Map<String, dynamic> json) {
     return ContactStatus(
-      statusId: json['status_id'],
-      tenantId: json['tenant_id'],
+      statusId: json['statusId'],
+      tenantId: json['tenantId'],
       status: json['status'],
-      isActive: json['is_active'],
+      isActive: json['isActive'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'status_id': statusId,
-      'tenant_id': tenantId,
+      'statusId': statusId,
+      'tenantId': tenantId,
       'status': status,
-      'is_active': isActive,
+      'isActive': isActive,
     };
   }
 }
